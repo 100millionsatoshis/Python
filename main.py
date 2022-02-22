60bed4db03f7ff285f7a43b57bda574154843378
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-02-22 15:46:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-print("Hello world. Let me tell you a story.")
+print("Welcome to RGames.\nLet's start the game.\nFirst off we need to set \"you\" up")
 main_character_name = "Tom"
 main_character_age = "23"
-main_character_gender = "boy"
+main_character_gender = "Boy"
 main_character_race = "white"
 main_character_job = "handyman"
-main_character_body_type = "skinny"
+main_character_body_type = "SKiNNy"
 main_character_height = "tall"
+main_character_wealth = "100.50"
+wealth_currency = "usd"
 live_alone = False
 secondary_character_age = "51"
 secondary_character_name = "Amanda"
 secondary_character_relationship = "friend"
 print("Your name is " + main_character_name + ".")
-print("You are a " + main_character_age + " years old " + main_character_race + " " + main_character_gender)
+print("Your name is")
+print(len(main_character_name))
+print("characters long")
+print('You are a ' + main_character_age + ' years old ' + main_character_race + ' ' + main_character_gender.lower())
+print("Therefore when you use pubic toilet, search for")
+print(main_character_gender[0])
+print("sign on the door.")
+print("That is number")
+print(main_character_gender.index("B"))
+print("letter of your gender.")
 print("You work as a " + main_character_job + ".")
-print("You are " + main_character_body_type + " and " + main_character_height + " ")
+print("You are " + main_character_body_type.lower() + " and " + main_character_height + " ")
+print("You have " + main_character_wealth + " " + wealth_currency.upper() + " in your bank account.")
+print("You wrote currency with capital letters.")
+print(wealth_currency.isupper())
+print(main_character_body_type.lower().islower())
 print("You live with " + secondary_character_age + " years old woman")
 print("Her name is " + secondary_character_name + " and she is your " + secondary_character_relationship + ".")
+print(main_character_gender.replace("Boy", "Girl"))
