d95c7174bad1a3a2bab57769df00c4b43408d16c
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-02-28 18:17:03 +0400

    Create list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+activities = ["museum", "movie", "swimming", "hiking", "bar", "restaurant"]
+print("Do you want to go to museum with me " + activities[1])
+print("I am going to " + activities[-2] + ". Do you want to join me?")
+print("How about you and me go to " + str(activities[1:]))
+print("Would you like to go to " + str(activities[2:4]) + ' with me?')
+activities[4] = "sushi bar"
+print(activities[4])
