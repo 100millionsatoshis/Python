0018e5493ad23888c803c487d2cf7291a005ab05
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-12 12:11:46 +0400

    Update and rename basiccalculator.py to 07_basic_calculator.py

--- 07_basic_calculator.py
+++ 07_basic_calculator.py
@@ -2,3 +2,7 @@ numb1 = input("Input first number: ")
 numb2 = input("Input second number: ")
 result = float(numb1) + float(numb2)
 print(result)
+
+# int turns string into a whole number.
+# float turns the string into a decimal number.
+
