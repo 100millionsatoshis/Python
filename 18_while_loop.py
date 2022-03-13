849c87231b1813241353387038276e76e9ff500e
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-13 16:56:46 +0400

    Create 18_while_loop.py

---
+++ 18_while_loop.py
@@ -0,0 +1,17 @@
+# while loop is basically a structure in python which allows us loop through and execute a block of code multiple times.
+# we could specify a few different lines of code and put that code inside of while loop, and it would loop through that code
+# executing it repeatedly until certain condition was false.
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+print("Done with loop")
+
+# i = i + 1  can be written as i += 1 which gives similar effect.
+
+i = 11
+while i <= 20:
+    print(i)
+    i += 1
+print("Done with loop")
