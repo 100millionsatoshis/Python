61b12d9f327d54de5e781bb23829134804baa606
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-21 17:11:36 +0400

    Update 26_reading_files.py

--- 26_reading_files.py
+++ 26_reading_files.py
@@ -8,7 +8,27 @@
 # One more mode is # r+ which means read and write
 # Now we save file as variable
 
-countries = open("countries.txt")
-# once you open a file, make sure to close it once you are done with it.
+countries = open("countries.txt", "r")
+print(countries)
+# this did not work.
+# now we make sure if file is readable.
+print(countries.readable())
 
+# once you open a file, make sure to close it once you are done with it.
+countries.close()
+
+# below I show an example of readable giving us false result due to file open mode is w
+# countries_and_its_capitals = open("countries.txt", "w")
+# print(countries_and_its_capitals.readable())
+# countries.close()
+
+countries_and_its_capitals = open("countries.txt", "r")
+print(countries_and_its_capitals.read())
+countries.close()
+
+print("we can read first line by using readline function")
+
+countries_and_its_capitals = open("countries.txt", "r")
+print(countries_and_its_capitals.readline())
+print(countries_and_its_capitals.readline())
 countries.close()
