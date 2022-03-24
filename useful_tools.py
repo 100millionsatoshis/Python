6494d22682d2859c25c44d1debf8b390fc96e339
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-24 13:42:57 +0400

    Add files via upload

---
+++ useful_tools.py
@@ -0,0 +1,16 @@
+import random
+
+feet_in_mile = 5280
+meters_in_kilometers = 1000
+beatles = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"]
+
+
+def get_file_ext(filename):
+    return filename[filename.index(".") + 1:]
+
+
+def roll_dice(num):
+    return random.randint(1, num)
+
+
+print(roll_dice(5))
