26658775acdddd1c79309bd5e4d8fa664326e724
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-24 13:10:28 +0400

    Add files via upload

---
+++ 28_modules_and_pips.py
@@ -0,0 +1,6 @@
+# module is a python file that we can import into our current python file
+# if we wrote a python file that has a bunch of useful functions and variables we could take it and import it
+# to the file we are currently working on.
+
+import useful_tools
+print(useful_tools.roll_dice(10))
