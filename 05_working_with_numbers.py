2093d402b66e5cd94eb1ab56f1ca60efce1f0b07
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-27 12:57:01 +0400

    Add files via upload

--- 05_working_with_numbers.py
+++ 05_working_with_numbers.py
@@ -3,7 +3,7 @@
 print(7)
 print(7.4677)
 print(-7.25)
-# "+" is python arithmetic operator called addition.
+# "+" is python arithmetic operator called addition. 7 and 3.5 are called operands.
 print(7 + 3.5)
 # "-" is python arithmetic operator called subtraction
 print(7 - 9.3)
@@ -11,13 +11,14 @@ print(7 - 9.3)
 print(12 * 12)
 # "/" forward slash is an arithmetic operator called division
 print(10 / 3)
-# "%" percentage sign is an arithmetic operator called modulo. The Python modulo operator calculates the remainder
-# of dividing two values. The first number is divided by the second then the remainder is returned.
+# "%" (percentage sign) is an arithmetic operator called modulo. (sometimes it is called remainder)
+# The Python modulo operator calculates the remainder of dividing two values. The first number is divided
+# by the second then the remainder is returned.
 print(10 % 3)
-# "**" is an arithmetic operator called exponentiation. It takes 1st number and multiplies it as much times as
+# "**" is an arithmetic operator called exponentiation. It takes 1st number and multiplies it as many times as
 # indicated in the 2nd number.
 print(2**3)
-# "//" is an arithmetic operator called floor division
+# "//" is an arithmetic operator called floor division or quotient.
 print("floor division 9//3 =", 9//3)
 # arithmetic operators are executed "first came, first served" basis
 print(2 * 3 + 4)
