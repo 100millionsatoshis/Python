a9ca30b953dead0bbe0893541b513cbd8d79d817
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-28 16:35:38 +0400

    Add files via upload

---
+++ 29_object.py
@@ -0,0 +1,7 @@
+from studentclass import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("Pam", "Art", 2.5, True)
+print(student1.name)
+print(student1.gpa)
+print(student2.gpa)
---
+++ studentclass.py
@@ -0,0 +1,8 @@
+# we can crete our own data types. With class, we define our own datatype.
+class Student:
+
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
