cc71653d2259831ac0e09fe2f977332b9fe84623
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-31 13:28:30 +0400

    Update 06_user_input.py

--- 06_user_input.py
+++ 06_user_input.py
@@ -1,7 +1,13 @@
+# Python allows for user input. That means we are able to ask user for input.
+# input function prompts the user for input, and returns what they enter as sting (with contents automatically
+# escaped).
+level = input("what is your level?: ")
 name = input("Enter your name: ")
 
-# inside the parenthesis we write prompt
+# inside the parenthesis we write prompt. Let's assume we want to take the age of the user as input.
+# We know input() function returns a string. To convert it to a number, we can use the int() function.
 
-age = input("Enter your age: ")
+age = int(input("Enter your age: "))
 
-print("Hello " + name + "! You are " + age + " years old")
+
+print("Hello " + name + "! You are " + age + " years old. You level is: " + level)
