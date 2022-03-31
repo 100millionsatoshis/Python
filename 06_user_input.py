a425671ba421acdede68a70eff7096bc72d4ae22
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-31 14:01:43 +0400

    Update 06_user_input.py

--- 06_user_input.py
+++ 06_user_input.py
@@ -10,4 +10,14 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
 
-print("Hello " + name + "! You are " + age + " years old. You level is: " + level)
+print("Hello " + name + "! You are " + str(age) + " years old. You level is: " + level)
+
+# walrus operator := allows you to assign values to variables within an expression, including variables that do not 
+# exist yet. 
+# instead of this: 
+"""
+color = str(input("what is your favorite color? "))
+print(color)
+"""
+# you write this: 
+print(color := str(input("what is your favorite color? ")))
