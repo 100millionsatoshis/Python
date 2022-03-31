7417f16008641cf7fda2f3de4a8aa19d673d4225
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-03-31 16:30:00 +0400

    Add files via upload

---
+++ Comparison Operators.py
@@ -0,0 +1,15 @@
+""" Comparison operators are:
+==  equal
+!=  not equal
+>   greater than
+<   smaller than
+>=  greater than or equal to
+<=  smaller than or equal to
+"""
+
+print("10 is greater than 5: ", 10 > 5)
+
+# you can compare string too. (lexicographically)
+
+print("Alphabetical number of a letter \"a\" is greater than alphabetical number of a letter \"d\": ", "a" > "d")
+print("Adam" < "Ann")
