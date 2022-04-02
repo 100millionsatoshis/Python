0c8fc01c0bd1a65d9085a08957d715d1b5bafcd1
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-04-02 16:16:06 +0400

    Update 09_list.py

--- 09_list.py
+++ 09_list.py
@@ -1,3 +1,6 @@
+"""
+Lists are used to store items. A list is created using square brackets with commas separating items.
+"""
 activities = ["museum", "movie", "swimming", "hiking", "bar", "restaurant"]
 print("Do you want to go to museum with me " + activities[1])
 print("I am going to " + activities[-2] + ". Do you want to join me?")
@@ -5,3 +8,20 @@ print("How about you and me go to " + str(activities[1:]))
 print("Would you like to go to " + str(activities[2:4]) + ' with me?')
 activities[4] = "sushi bar"
 print(activities[4])
+"""
+Sometimes you need to create an empty list and populate it later during the program. For example, if you are creating 
+a queue management program, the queue is going to be empty in the beginning and get populated with people data later. 
+An empty list is created with an empty pair of square brackets.  
+"""
+
+empty_list = []
+"""
+# In some code samples you might see a comma after the last item in the list. It is not mandatory, but perfectly valid.
+Typically, a list will contain items of a single item type, but it is also possible to include several different types. 
+Lists also can be nested within other lists. 
+"""
+number = 3
+things = ["string", 0, [1, 2, number], 4.56]
+print(things[1])
+print(things[2])
+print(things[2][2])
