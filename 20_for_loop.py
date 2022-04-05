3b351b86f31405d1aa41dac15846566020b5de59
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-04-05 21:28:54 +0400

    Update 20_for_loop.py

--- 20_for_loop.py
+++ 20_for_loop.py
@@ -36,3 +36,4 @@ for letter in phrase:
     if letter == "a":
         count += 1
 print(count)
+# smiliar to while loops, the break and continue statements can be used in for loops to stop the loop or jump into next iteration. 
