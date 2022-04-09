da82efe042b2b95af8b3b6a88b040a0d627d6595
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-04-09 11:47:58 +0400

    Add files via upload

---
+++ 24_comments.py
@@ -0,0 +1,15 @@
+# we use hashtag (octothorpe) to write comments inside python code, and it is not executed by python.
+# we write comments to leave some note, or little reminder or line to explain a line of code
+# trippe quotation mar is another way to write a comment. It is called docstrings (documentation strings)
+#
+'''
+print("THis is comment") This is docstring. Unlike conventional comments, docstrings are retained throughout the
+runtime of the program. This allows the programmer to inspect these comments at run time.  
+'''
+
+# sometimes commenting can be useful to commenting out the line out code to disable it instead of deleting.
+
+print('Let us say this line of code causing you trouble')
+
+# by putting hashtag in front of it you can disable it.
+# print("Let's say this line of code causing you trouble")
