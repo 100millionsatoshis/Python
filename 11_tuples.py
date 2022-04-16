2c73f5399e5a6928c45c9be65394e4e60bcf731f
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-04-16 20:00:54 +0400

    Update 11_tuples.py

--- 11_tuples.py
+++ 11_tuples.py
@@ -1,11 +1,11 @@
-#A Tuple is a type of data structure which basically means container where we can store different values.
+# A Tuple is a type of data structure which basically means container where we can store different values.
 # If you are familiar with lists in python, tuple is very similar to a list. It is basically a structure where we can
-#store a multiple pieces of information. But a tuple has a couple key differences from lists.
-#One of the most common examples of tupples is coordinates.
+# store a multiple pieces of information. But a tuple has a couple key differences from lists.
+# One of the most common examples of tuples is coordinates.
 
 coordinates = (2000, 178, 80)
 
-#Tuple is immutable which means a tuple cannot be changed or modified
+# Tuple is immutable which means a tuple cannot be changed or modified
 
 print(coordinates)
 print(coordinates[0])
