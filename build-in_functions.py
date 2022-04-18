ba1f6679c95b8dae563c577588589f7516da6c9a
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-04-18 13:02:15 +0400

    Add files via upload

---
+++ build-in_functions.py
@@ -0,0 +1,8 @@
+# abs()	Returns the absolute value of a number
+x = abs(-7.25)
+print(x)
+y = abs(3+5j)
+print(y)
+print(isinstance(y, float))
+
+# build-in abs() function may not always return float value. This needs more clarification.
