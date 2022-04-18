db4d8f69c79a77088a1bfe00d40c5917d856ee4a
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-04-18 11:14:09 +0400

    Create module.datetime.py

---
+++ module.datetime.py
@@ -0,0 +1,10 @@
+# A date in Python is not a data type of its own, but we can import a module named datetime to work with dates as
+# date objects.
+# Import the datetime module and display the current date:
+import datetime
+x = datetime.datetime.now()
+print(x)
+
+# Return the year and name of weekday:
+print(x.year)
+print(x.strftime("%A"))
