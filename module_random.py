e5e89363589401346c8dfbb8d788687c4ea2d428
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-04-23 21:06:40 +0400

    Update module_random.py

--- module_random.py
+++ module_random.py
@@ -7,3 +7,19 @@ print(random.random())
 # By default, the random number generator uses the current system time.
 # Use the seed() method to customize the start number of the random number generator.
 # Note: If you use the same seed value twice you will get the same random number twice.
+
+# getstate() Returns the current internal state of the random number generator
+print(random.getstate())
+# The getstate() method returns an object with the current state of the random number generator.
+# Use this method to capture the state, and use the setstate() method, with the captured state, to restore the state
+
+# setstate() Restores the internal state of the random number generator.
+print(random)
+state = random.getstate()
+print(random.random())
+random.setstate(state)
+print(random.random())
+
+# getrandbits() Returns a number representing the random bits.
+print("This is an 8 bits sized integer:", random.getrandbits(8))
+# returns an 8 bit sized integer
