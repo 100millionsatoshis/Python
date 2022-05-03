e6612332c245a45d4581110187347980188132e2
100millionsatoshis <99354029+100millionsatoshis@users.noreply.github.com>
2022-05-03 23:56:19 +0400

    Add files via upload

--- build-in_functions.py
+++ build-in_functions.py
@@ -6,3 +6,11 @@ print(y)
 print(isinstance(y, float))
 
 # build-in abs() function may not always return float value. This needs more clarification.
+
+# float() Returns a floating point number
+a = float(3)
+print("3 converted into floating point number is", a)
+# A number or a string that can be converted into a floating point number.
+print(float("-3.5"))
+# String can be converted only a number is stored as string
+print(float("inf"))
